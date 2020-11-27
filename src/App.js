9f39bc0abdbba0f269714dccc79221ac8d2764a2
Atabic <atabic14@gmail.com>
2020-11-27 18:47:45 +0500

    Portfolio theme added only

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 // import LandingPage from './components/LandingPage';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
+    {/* <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Hola Everyone! The Website is under development<br/>
       </p>
-    </header>
+    </header> */}
   </div>
   );
 }
