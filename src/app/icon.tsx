ad300097ca596b2035ef99091d574e3d5b8da02a
Atabic <atabic14@gmail.com>
2025-02-12 04:16:06 +0500

    added icon

---
+++ src/app/icon.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from "next/og";
+import Image from "next/image";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const icon = "/me.png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 14,
+          color: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "black",
+        }}
+      >
+        A
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
